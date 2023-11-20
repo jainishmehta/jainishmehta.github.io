3c6862ac046de9ad27464102bfd7b49c93cc735c
jainish <jainishmehta>
2023-11-20 11:04:35 -0700

    Added blog

--- src/components/blog.js
+++ src/components/blog.js
@@ -13,7 +13,7 @@ class Blog extends Component {
                             <Fade top cascade>
                                 <h1 style={{textAlign:'center', font:'Times New Roman'}}>
                                     <i>
-                                        <strong>Coming soon...</strong>
+                                        <strong>Check out my blog!</strong>
                                     </i>
                                 </h1>
                                 <a href="https://medium.com/@jainishmehta09" rel="noopener noreferrer" target="_blank">
